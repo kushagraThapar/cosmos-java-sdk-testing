a78da61ce9afad8e05cc7bbc3e6b0df1f132fd2f
Kushagra Thapar <kuthapar@microsoft.com>
2022-05-08 14:43:03 -0700

    Added flux hang and flux no hang without timeout

--- src/main/java/com/example/common/FluxHangIssue.java
+++ src/main/java/com/example/common/FluxHangIssue.java
@@ -14,7 +14,49 @@ public class FluxHangIssue {
     private final static Logger logger = LoggerFactory.getLogger(FluxHangIssue.class);
 
     public static void main(String[] args) throws InterruptedException {
+        fluxHang();
 
+        fluxWithTimeoutNoHang();
+    }
+
+    public static void fluxHang() throws InterruptedException {
+        Flux<Integer> integerFlux = Flux.range(0, 7).map(number -> {
+            logger.info("Number is : {}", number);
+            if (number > 5) {
+                throw new OutOfMemoryError("Custom GC Failure");
+            }
+            return number;
+        }).doOnError(ex -> {
+            logger.error("Completed exceptionally", ex);
+        }).doOnNext(next -> {
+            logger.info("Next is : {}", next);
+        }).doOnComplete(() -> {
+            logger.info("Completed successfully");
+        }).doFinally(signalType -> {
+            logger.info("Finally signal is : {}", signalType);
+        }).onErrorMap(throwable -> {
+            logger.info("On error map", throwable);
+            return throwable;
+        }).onErrorContinue((throwable, object) -> {
+            logger.error("on error continue : {}", object, throwable);
+        }).onErrorStop().onErrorReturn(6).onErrorResume(throwable -> {
+            logger.info("on error resume", throwable);
+            return Mono.error(throwable);
+        }).subscribeOn(Schedulers.boundedElastic());
+
+        Iterator<Integer> integers = integerFlux.toIterable().iterator();
+        while(integers.hasNext()) {
+            logger.info("Next value is : {}", integers.next());
+        }
+
+        logger.info("Going to sleep now");
+
+        Thread.sleep(5000);
+
+        logger.info("I woke up");
+    }
+
+    public static void fluxWithTimeoutNoHang() throws InterruptedException {
         Flux<Integer> integerFlux = Flux.range(0, 7).map(number -> {
             logger.info("Number is : {}", number);
             if (number > 5) {
