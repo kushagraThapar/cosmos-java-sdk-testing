f15ec48e5d6c5bebbb241ac59253f0bb8173eff7
Kushagra Thapar <kuthapar@microsoft.com>
2022-09-26 15:22:38 -0700

    added example for java time module

--- src/main/java/com/example/common/CosmosBulkExampleWithSystemKey.java
+++ src/main/java/com/example/common/CosmosBulkExampleWithSystemKey.java
@@ -4,19 +4,19 @@ import com.azure.cosmos.CosmosAsyncClient;
 import com.azure.cosmos.CosmosAsyncContainer;
 import com.azure.cosmos.CosmosAsyncDatabase;
 import com.azure.cosmos.CosmosClientBuilder;
-import com.azure.cosmos.implementation.Utils;
+import com.azure.cosmos.models.CosmosBulkOperations;
+import com.azure.cosmos.models.CosmosItemOperation;
 import com.azure.cosmos.models.CosmosItemRequestOptions;
 import com.azure.cosmos.models.CosmosItemResponse;
 import com.azure.cosmos.models.PartitionKey;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
 
-import java.time.Instant;
+public class CosmosBulkExampleWithSystemKey {
 
-public class CosmosCRUDExample {
-
-    private static final Logger logger = LoggerFactory.getLogger(CosmosCRUDExample.class);
+    private static final Logger logger = LoggerFactory.getLogger(CosmosBulkExampleWithSystemKey.class);
 
     public static void main(String[] args) {
         CosmosAsyncClient cosmosAsyncClient = new CosmosClientBuilder()
@@ -25,39 +25,39 @@ public class CosmosCRUDExample {
             .contentResponseOnWriteEnabled(true)
             .buildAsyncClient();
 
-//        Utils.getSimpleObjectMapper().registerModule(new JavaTimeModule());
-
         CosmosAsyncDatabase cosmosAsyncDatabase = cosmosAsyncClient.getDatabase("testdb");
         CosmosAsyncContainer cosmosAsyncContainer = cosmosAsyncDatabase.getContainer("testcontainer");
 
 
-        Family family = new Family("id-5", "Kushagra", "Thapar", "pk-3", Instant.now());
+        Family family = new Family("id-4", "Kushagra", "Thapar", "pk-3");
         logger.info("Creating Item");
-        cosmosAsyncContainer.createItem(family, new PartitionKey(family.getMyPk()), new CosmosItemRequestOptions()).block();
+        cosmosAsyncContainer.createItem(family, new PartitionKey(family.get_partitionKey()), new CosmosItemRequestOptions()).block();
         logger.info("Item created");
 
-        logger.info("Reading item");
-        CosmosItemResponse<Family> cosmosItemResponse = cosmosAsyncContainer.readItem(family.getId(),
-            new PartitionKey(family.getMyPk()), Family.class).block();
+        logger.info("Creating items through bulk");
+        family = new Family("id-5", "Kushagra", "Thapar", "pk-4");
+        CosmosItemOperation createItemOperation = CosmosBulkOperations.getCreateItemOperation(family,
+            new PartitionKey(family.get_partitionKey()));
+        cosmosAsyncContainer.executeBulkOperations(Flux.just(createItemOperation)).blockLast();
+        logger.info("Items through bulk created");
 
-        logger.info("Cosmos Item response is : {}", cosmosItemResponse.getItem());
+        CosmosItemResponse<JsonNode> cosmosItemResponse = cosmosAsyncContainer.readItem("075a9793-d096-4e6f"
+            + "-83b4-c774d740f54a", PartitionKey.NONE, JsonNode.class).block();
+
+        logger.info("Cosmos Item response is : {}", cosmosItemResponse.getItem().toPrettyString());
     }
 
     private static class Family {
         private String id;
         private String firstName;
         private String lastName;
-        private String myPk;
-        private Instant time;
+        private String _partitionKey;
 
-        public Family(){}
-
-        public Family(String id, String firstName, String lastName, String myPk, Instant time) {
+        public Family(String id, String firstName, String lastName, String _partitionKey) {
             this.id = id;
             this.firstName = firstName;
             this.lastName = lastName;
-            this.myPk = myPk;
-            this.time = time;
+            this._partitionKey = _partitionKey;
         }
 
         public String getId() {
@@ -84,31 +84,12 @@ public class CosmosCRUDExample {
             this.lastName = lastName;
         }
 
-        public String getMyPk() {
-            return myPk;
+        public String get_partitionKey() {
+            return _partitionKey;
         }
 
-        public void setMyPk(String myPk) {
-            this.myPk = myPk;
-        }
-
-        public Instant getTime() {
-            return time;
-        }
-
-        public void setTime(Instant time) {
-            this.time = time;
-        }
-
-        @Override
-        public String toString() {
-            return "Family{" +
-                "id='" + id + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", pk='" + myPk + '\'' +
-                ", time=" + time +
-                '}';
+        public void set_partitionKey(String _partitionKey) {
+            this._partitionKey = _partitionKey;
         }
     }
 }
