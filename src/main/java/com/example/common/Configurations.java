96377a138286adf08cb01ff3011458290f58deb0
Abhijeet Mohanty <abhmohanty@microsoft.com>
2023-08-16 23:46:25 -0400

    Corrected code comments.

--- src/main/java/com/example/common/Configurations.java
+++ src/main/java/com/example/common/Configurations.java
@@ -3,6 +3,6 @@ package com.example.common;
 public class Configurations {
 
 
-    public final static String endpoint = "https://abhm-cosmosdb-account.documents.azure.com:443/";
-    public final static String key = "wowIRKt1qXl1qWP4tPfUxSyDTbNdzhIP0d5tz8gKeHYGLVXlPAYhiCYYSyBOUFaJ07ExXMj6NZQdACDbnAXggQ==";
+    public final static String endpoint = "endpoint";
+    public final static String key = "key";
 }
